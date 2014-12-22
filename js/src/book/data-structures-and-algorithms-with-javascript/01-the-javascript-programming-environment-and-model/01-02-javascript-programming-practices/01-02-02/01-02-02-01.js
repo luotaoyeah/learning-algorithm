c24f255aa732127fa372867eddad460ded043efa
luotao <luotaoyeah@gmail.com>
2014-12-22 16:38:01 +0800

    js: javascript: arithmetic operator

---
+++ js/src/book/data-structures-and-algorithms-with-javascript/01-the-javascript-programming-environment-and-model/01-02-javascript-programming-practices/01-02-02/01-02-02-01.js
@@ -0,0 +1,24 @@
+/*
+ * The JavaScript Programming Environment and Model
+ *     JavaScript Programming Practices
+ *         Arithmetic and Math Library Functions in JavaScript
+ *             arithmetic operator
+ */
+
+console.log("\n-------------------------------------------------- 01");
+(function() {
+  /*
+   * js 支持标准的算术运算：
+   *     + 加法
+   *     - 减法
+   *     * 乘法
+   *     / 除法
+   *     % 取余
+   */
+
+  console.log(9 + 2); // 11
+  console.log(9 - 2); // 7
+  console.log(9 * 2); // 18
+  console.log(9 / 2); // 4.5
+  console.log(9 % 2); // 1
+})();
