277bf7022b09314fd252df680539ca738d763edd
luotao <luotaoyeah@gmail.com>
2014-12-25 19:49:36 +0800

    js: array: String.prototype.split()

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-02-using-arrays/02-02-02/02-02-02-01");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-02-using-arrays/02-02-03/02-02-03-01");
