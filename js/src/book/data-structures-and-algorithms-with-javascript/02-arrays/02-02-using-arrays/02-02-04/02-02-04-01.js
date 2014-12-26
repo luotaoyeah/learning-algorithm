24978d9b4ea02dfe5b2a97df79cf722ec8077481
luotao <luotaoyeah@gmail.com>
2014-12-26 21:39:48 +0800

    js: array: aggregate array operation

---
+++ js/src/book/data-structures-and-algorithms-with-javascript/02-arrays/02-02-using-arrays/02-02-04/02-02-04-01.js
@@ -0,0 +1,23 @@
+/*
+ * Arrays
+ *     Using Arrays
+ *         Aggregate Array Operations
+ */
+
+console.log("\n-------------------------------------------------- 01");
+(function() {
+  /*
+   * 当把一个 array 赋值给其他 variable 时，复制的是 array 的 reference
+   * 即，进行的是浅拷贝（shallow copy）
+   */
+
+  var arr01 = [1, 2, 3];
+  var arr02 = arr01;
+
+  arr01[1] = 9;
+  arr02[2] = 99;
+
+  console.log(arr01); // [ 1, 9, 99 ]
+  console.log(arr02); // [ 1, 9, 99 ]
+  console.log(arr01 === arr02); // true
+})();
