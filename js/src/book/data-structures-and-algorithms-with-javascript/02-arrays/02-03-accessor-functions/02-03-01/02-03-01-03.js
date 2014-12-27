d5822cd84fe60ecd7def300cb71f335c34d88b93
luotao <luotaoyeah@gmail.com>
2014-12-27 18:29:47 +0800

    js: array: Array.prototype.lastIndexOf()

---
+++ js/src/book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-01/02-03-01-03.js
@@ -0,0 +1,23 @@
+/*
+ * Arrays
+ *     Accessor Functions
+ *         Searching for a Value
+ *             Array.prototype.indexOf()
+ *             Array.prototype.lasIndexOf()
+ */
+
+console.log("\n-------------------------------------------------- 01");
+(function() {
+  /*
+   * Array.prototype.indexOf() 方法，
+   * Array.prototype.lastIndexOf() 方法，
+   *     第二个参数表示：从哪个索引开始查找，默认为 length - 1
+   */
+  var arr = [1, 2, 3, 2, 1];
+
+  console.log(arr.lastIndexOf(2)); // 3
+  console.log(arr.lastIndexOf(2, 2)); // 1
+
+  console.log(arr.indexOf(2)); // 1
+  console.log(arr.indexOf(2, 2)); // 3
+})();
--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-01/02-03-01-02");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-01/02-03-01-03");
