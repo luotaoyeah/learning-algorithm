d58e24f083069259cb8f82a332df731048f64631
luotao <luotaoyeah@gmail.com>
2014-12-27 18:25:57 +0800

    js: array: Array.prototype.lastIndexOf()

---
+++ js/src/book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-01/02-03-01-02.js
@@ -0,0 +1,20 @@
+/*
+ * Arrays
+ *     Accessor Functions
+ *         Searching for a Value
+ *             Array.prototype.lasIndexOf()
+ */
+
+console.log("\n-------------------------------------------------- 01");
+(function() {
+  /*
+   * Array.prototype.lastIndexOf() 方法，
+   * 检查参数是否存在于该数组中，
+   *     如果存在，则返回最后一次出现的索引，
+   *     如果不存在，则返回 -1
+   */
+  var arr = [1, 2, 3, 2, 1];
+
+  console.log(arr.lastIndexOf(2)); // 3
+  console.log(arr.lastIndexOf(9)); // -1
+})();
