3827c637547d197ad4bcde7b7658004ba42cbf83
luotao <luotaoyeah@gmail.com>
2014-12-27 18:35:28 +0800

    js: array: Array.prototype.join()

---
+++ js/src/book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-02/02-03-02-01.js
@@ -0,0 +1,21 @@
+/*
+ * Arrays
+ *     Accessor Functions
+ *         String Representations of Arrays
+ *             Array.prototype.join()
+ *
+ */
+
+console.log("\n-------------------------------------------------- 01");
+(function() {
+  /*
+   * Array.protytype.join() 方法，
+   * 将数组的所有元素使用指定的连接符连接成一个字符串，
+   * 默认的连接符为逗号（,）
+   */
+
+  var arr = [1, 2, 3];
+  console.log(arr.join()); // 1,2,3
+  console.log(arr.join("")); // 123
+  console.log(arr.join("-")); // 1-2-3
+})();
