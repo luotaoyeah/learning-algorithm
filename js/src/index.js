c9848aee66103722007117773bab48537cb5c563
luotao <luotaoyeah@gmail.com>
2014-12-28 21:32:15 +0800

    js: array: Array.prototype.concat()

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-02/02-03-02-02");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-03-accessor-functions/02-03-03/02-03-03-01");
