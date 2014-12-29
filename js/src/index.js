71dd5598a36b9217bbb7eea42367cadcabe476e1
luotao <luotaoyeah@gmail.com>
2014-12-29 21:14:28 +0800

    js: array: Array.prototype.unshift()

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-04-mutator-functions/02-04-01/02-04-01-01");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-04-mutator-functions/02-04-01/02-04-01-02");
