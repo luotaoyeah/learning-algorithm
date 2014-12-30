67154a788f80cd48ccfe4582d3f204fb980c407d
luotao <luotaoyeah@gmail.com>
2014-12-30 16:35:40 +0800

    js: array: Array.prototype.splice()

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-04-mutator-functions/02-04-03/02-04-03-01");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-04-mutator-functions/02-04-03/02-04-03-02");
