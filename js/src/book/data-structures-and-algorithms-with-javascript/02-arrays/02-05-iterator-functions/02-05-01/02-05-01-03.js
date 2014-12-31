2e183af5042c2b4fcaf3570622f8eb0b9d0d5f54
luotao <luotaoyeah@gmail.com>
2014-12-31 13:38:59 +0800

    js: array: Array.prototype.every()

---
+++ js/src/book/data-structures-and-algorithms-with-javascript/02-arrays/02-05-iterator-functions/02-05-01/02-05-01-03.js
@@ -0,0 +1,26 @@
+/*
+ * Arrays
+ *     Iterator Functions
+ *         Non–Array-Generating Iterator Functions
+ *             Array.prototype.every()
+ */
+
+console.log("\n-------------------------------------------------- 01");
+(function() {
+  /*
+   * Array.prototype.every() 方法，
+   * 用于判断：是否数组中的所有元素都满足某个条件
+   */
+
+  var arr = [2, 4, 6];
+
+  /*
+   * 回调函数接受三个参数：当前元素，当前索引，当前数组
+   * 回调函数返回 boolean 值，表示当前元素是否满足条件
+   */
+  console.log(
+    arr.every(function(value, index, array) {
+      return value % 2 === 0;
+    })
+  ); // true
+})();
--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-05-iterator-functions/02-05-01/02-05-01-02");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-05-iterator-functions/02-05-01/02-05-01-03");
