21cead45a9163d14fbae1958c0338da2f20fe7c3
luotao <luotaoyeah@gmail.com>
2015-01-01 11:11:10 +0800

    js: array: Array.prototype.filter()

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-05-iterator-functions/02-05-02/02-05-02-01");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-05-iterator-functions/02-05-02/02-05-02-02");
