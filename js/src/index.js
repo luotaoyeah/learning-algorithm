9dd5aefcc1a1626eb085f00217883c6bb4e1c337
luotao <luotaoyeah@gmail.com>
2015-01-01 11:24:46 +0800

    js: array: multi-dimensional array

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-05-iterator-functions/02-05-02/02-05-02-02");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-06-two-dimensional-and-multidimensional-arrays/02-06-01/02-06-01-01");
