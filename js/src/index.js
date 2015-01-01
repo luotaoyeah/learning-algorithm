0d1e514baadf68426a9fc71936e484321846f605
luotao <luotaoyeah@gmail.com>
2015-01-01 11:47:09 +0800

    js: array: processing two-dimensional array

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-06-two-dimensional-and-multidimensional-arrays/02-06-01/02-06-01-01");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-06-two-dimensional-and-multidimensional-arrays/02-06-02/02-06-02-01");
