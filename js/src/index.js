e08d57e309f103f8acb1d238591e7142d7e7c1a4
luotao <luotaoyeah@gmail.com>
2015-01-03 21:47:50 +0800

    js: array: array of objects

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,1 @@
-require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-06-two-dimensional-and-multidimensional-arrays/02-06-03/02-06-03-01");
+require("./book/data-structures-and-algorithms-with-javascript/02-arrays/02-07-arrays-of-objects/02-07-01/02-07-01-01");
